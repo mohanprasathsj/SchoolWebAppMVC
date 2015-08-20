08e7a85d2f9653a6d0136dd856438823407f98d6
Mohan Prasath S J <mohanprasathsj@gmail.com>
2015-08-20 19:32:06 +0800

    Made changes in Basic Files

--- StudentAppMVC/Models/SchoolClass.cs
+++ StudentAppMVC/Models/SchoolClass.cs
@@ -6,6 +6,11 @@ namespace StudentAppMVC.Models
 {
     public class SchoolClass : Basic
     {
+        public SchoolClass()
+        {
+            StandardId = Guid.Parse("E7B6C384-C1D5-489A-A727-6DF12B9FE41A");
+        }
+
         public virtual List<Subject> Subjects { get; set; }
 
         public virtual List<Student> Students { get; set; }
